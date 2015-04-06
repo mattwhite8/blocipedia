b2dda57e055eb559a4365e0ce06995af35c283b3
mattwhite8 <mattwhite1337@gmail.com>
2015-04-06 15:04:12 +0000

    added authorization

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,18 +1,22 @@
 class WikisController < ApplicationController
   def index
     @wikis = Wiki.all 
+    authorize @wikis
   end
 
   def show
     @wiki = Wiki.find(params[:id])
+    authorize @wiki
   end
 
   def new
     @wiki = Wiki.new
+    authorize @wiki
   end
   
   def create
     @wiki = Wiki.new(wiki_params)
+    authorize @wiki
     if @wiki.save
       redirect_to @wiki, notice: "Wiki created sucessfully"
     else
@@ -22,10 +26,12 @@ class WikisController < ApplicationController
 
   def edit
     @wiki = Wiki.find(params[:id])
+    authorize @wiki
   end
   
   def update
     @wiki = Wiki.find(params[:id])
+    authorize @wiki
     if @wiki.update_attributes(wiki_params)
       redirect_to @wiki, notice: "Wiki updated sucessfully"
     else
@@ -37,6 +43,7 @@ class WikisController < ApplicationController
   def destroy 
     @wiki = Wiki.find(params[:id])
     title = @wiki.title
+    authorize @wiki
     if @wiki.destroy
       flash[:notice] = "\"#{title}\" was deleted sucessfully"
       redirect_to wikis_path

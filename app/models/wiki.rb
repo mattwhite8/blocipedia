3e35eaa1477b4118ea050d3bf63a38ae85e905d0
mattwhite8 <mattwhite1337@gmail.com>
2015-04-08 01:20:20 +0000

    Enabled private topics for Premium / Admins

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,3 +1,8 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
+  
+  
+  scope :visible_to, -> (user) { user.role == 'admin' || user.role == 'premium' ? all : where(private: false) }
+  
+  
 end
